ace59f713031fd0c1d81bd7a17c9a46ba243592e
Christian Burkhart <burkhart.christian@gmail.com>
2016-08-30 14:21:17 +0200

    minor improvements

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -175,23 +175,6 @@ gulp.task('serve', ['scripts', 'styles'], () => {
     port: 3000,
     server: {
       baseDir: ['.tmp', 'app'],
-      middleware: function (req, res, next) {
-
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'https://open-api.bahn.de');
-
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        
-        next();
-      }
     }
   });
 
